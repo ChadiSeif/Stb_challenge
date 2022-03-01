64199d23d4b5500e1419846fdb2b46a3c48cf0ee
ChadiSeif <84188405+ChadiSeif@users.noreply.github.com>
2022-03-01 12:49:18 +0100

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,25 +14,19 @@ const NavBar = () => {
         }}
       >
         <Container>
-          <Navbar.Brand href="/home" style={{ color: "white" }}>
+          <Navbar.Brand href="/" style={{ color: "white" }}>
             <img src="/assets/FINALLOGOwhite.png" alt="logo" width="150px" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              {/* <Nav.Link style={{ color: "white" }} ">
+              {/* <Nav.Link style={{ color: "white" }} href="/nous-connaitre">
                 Nous connaitre
               </Nav.Link> */}
-              <Nav.Link style={{ color: "white" }}">
-                Présentation
-              </Nav.Link>
-              <Nav.Link style={{ color: "white" }} >
-                Nos Chiffres clés
-              </Nav.Link>
-              <Nav.Link style={{ color: "white" }} >
-                Notre équipe
-              </Nav.Link>
-              <Nav.Link style={{ color: "white" }} >
+              <Nav.Link style={{ color: "white" }}>Présentation</Nav.Link>
+              <Nav.Link style={{ color: "white" }}>Nos Chiffres clés</Nav.Link>
+              <Nav.Link style={{ color: "white" }}>Notre équipe</Nav.Link>
+              <Nav.Link style={{ color: "white" }}>
                 Responsabilite sociale
               </Nav.Link>
             </Nav>
